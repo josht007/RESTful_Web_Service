a057149dd57c31b44b7daffb92a4bdd22588437f
josht007 <148504666+josht007@users.noreply.github.com.>
2025-05-02 23:24:04 -0400

    first changes

---
+++ src/main/java/com/josht00/restful_web_service/ResTfulWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.josht00.restful_web_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResTfulWebServiceApplication.class, args);
+    }
+
+}
